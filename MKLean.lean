dbf094824d330cdff07c2591e4ed152f843b247e
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-07-18 14:36:45 +0200

    Throw errors

--- MKLean.lean
+++ MKLean.lean
@@ -7,15 +7,15 @@ import MKLean.Update
 
 
 def main (argv : List String) : IO Unit :=
-  let usage := "Usage: mklean [update | <project directory> [math?]]"
+  let usage := "Usage: mklean {update | <project directory> [math?]}"
 
   let argv := argv.toArray
   if argv.size < 1 then
-    IO.println s!"Error: no project directory specified.\n{usage}."
+    throw <| IO.userError s!"Error: no project directory specified.\n{usage}."
   else
     match argv with
     | #["update"] => update
     | #[n] => make_lean_project n false
     | #[n, "math"]
     | #[n, "m"] => make_lean_project n true
-    | _ => IO.println s!"Error: too many arguments.\n{usage}."
+    | _ => throw <| IO.userError s!"Error: too many arguments.\n{usage}."
